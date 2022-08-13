a3516c6c332622c30f52e8aab875587f098c5909
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-08-13 22:01:28 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const os = require("node:os")
+const WebSocket = require("ws");
+class Constellar {
+  constructor(name) {
+    this.name = name || null
+  }
+
+  start() {
+    if (this.name) {
+      this.ws = new WebSocket("wss://JNHdujzjPpkf.sachikushanakarimi.repl.co");
+      this.ws.onopen = () => {
+        setInterval(function() {
+          this.ws.send(JSON.stringify({
+            name: this.name,
+            cpu: os.cpu(),
+            memoryusage: os.totalmem() - os.freemem(),
+            memoryfree: os.freemem(),
+            os: os.type()
+          }));
+        })
+        console.log('Constellar Succesfull To Connect Websocket');
+      }
+      this.ws.onclose = this.ws.onerror = (e) => {
+        this.ws = null
+      }
+    } else {
+      console.log("Your Session Is Rejected.")
+    }
+  }
+}
+
+module.exports = Constellar
